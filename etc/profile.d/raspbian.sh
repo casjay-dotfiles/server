53756cb93b498e346a81cf8c12eb1dc426b64615
Jason <jason@casjay.net>
2021-07-31 23:39:36 -0400

    ✔️🐛 Changed: master branch to main 🐛✔️

--- etc/profile.d/raspbian.sh
+++ etc/profile.d/raspbian.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-alias updateconfigs="sudo update=yes bash -c "$(curl -LsS https://github.com/casjay-dotfiles/server/raw/master/install.sh)""
+alias updateconfigs="sudo update=yes bash -c "$(curl -LsS https://github.com/casjay-dotfiles/server/raw/main/install.sh)""
