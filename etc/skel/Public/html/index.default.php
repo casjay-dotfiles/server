706bd8982a1e96e4c44adb33b35defddd5868807
casjay <git-admin@casjaysdev.pro>
2024-06-23 10:45:06 -0400

    🗃️ Committing everything that changed 🗃️
    index.default.php

--- etc/skel/Public/html/index.default.php
+++ etc/skel/Public/html/index.default.php
@@ -19,9 +19,12 @@ site owner is working on it!  <br><br><br><br>
 
   <div class="c3">
 Server Admin you can now upload your site to  <br>
-<?php echo $_SERVER['DOCUMENT_ROOT']; ?>  <br><br><br>
-<?php echo "Server Name: ".$_SERVER['SERVER_NAME']."<BR>"; ?>
-<?php echo "IP Address: ".$_SERVER['SERVER_ADDR']."<BR>"; ?>  <br>
+<?php echo $_SERVER['DOCUMENT_ROOT']; ?>
+<br><br><br>
+<?php echo "System Hostname: " , gethostname() . "<br />"; ?>
+<?php echo "Server Name: ".$_SERVER['SERVER_NAME']."<br />"; ?>
+<?php echo "IP Address: ".$_SERVER['SERVER_ADDR']."<br />"; ?>
+<br>
   </div>
 
 <center>
